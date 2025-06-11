662c97df72dfcfbaeb2964d10964177bae943fa0
david123mbah <davidmbah1234@gmail.com>
2025-06-11 17:11:35 +0100

    design docker vps files

--- frontend/src/Components/NotificationsProvider/NotificationsContext.jsx
+++ frontend/src/Components/NotificationsProvider/NotificationsContext.jsx
@@ -11,6 +11,7 @@ import {
 } from "firebase/firestore";
 import { db } from "../../../../firebase";
 
+
 const NotificationsContext = createContext();
 
 const NotificationsProvider = ({ children }) => {
