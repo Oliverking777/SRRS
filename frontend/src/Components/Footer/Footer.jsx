0bab225d45499a91d2391908e0e790a51a01eb9f
david123mbah <davidmbah1234@gmail.com>
2025-06-23 17:45:34 +0100

    frontend folder

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import { assets } from '../../assets/assets'
-import './Footer.css'
+import React from "react";
+import { assets } from "../../assets/assets";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="Footer">
       <div className="left_footer">
-        <img src={assets.heart_rate} alt="" />
-        <p> © 2025 SRRS. All rights reserved  davidmbah.</p>
+        <img src={assets.heart_rate} alt="SRRS Logo" />
+        <p>
+          {" "}
+          © 2025 SRRS. All rights reserved&nbsp;
+          <span style={{ color: "#4169e1", fontWeight: 600 }}>davidmbah</span>.
+        </p>
       </div>
       <div className="right_footer">
-        <a href="" className='pr'>Privacy Policy</a>
-        <a href="" className='term'>Terms of Service</a>
-        <a href="" className='contact'>Contact</a>
+        <a href="#" className="pr" aria-label="Privacy Policy">
+          Privacy Policy
+        </a>
+        <a href="#" className="term" aria-label="Terms of Service">
+          Terms of Service
+        </a>
+        <a href="#" className="contact" aria-label="Contact">
+          Contact
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
